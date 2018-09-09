8be2c2da8ed9a4a7716cbc2a5899fc855a8ff812
tbscanlon <thomas.scanlon@outlook.com>
2018-09-09 16:16:25 +0100

    Tidy up index

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,28 @@
 import express from "express";
 import mongoose from "mongoose";
+import { Server } from "http";
 
 import { products } from "./controllers/products";
+import { orders } from "./controllers/orders";
 
 const PORT = 8080;
 const MONGO_URL = "mongodb://tb:123secure@ds249992.mlab.com:49992/engaging-tech-test";
 
-// let db: mongoose.Connection;
 const app = express();
 
-mongoose.connect(MONGO_URL);
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
+const setupRoutes = () => {
+  app.use("/products", products);
+  app.use("/orders", orders);
+};
 
-app.use("/products", products);
+const setupDb = (url = MONGO_URL) => {
+  mongoose.connect(url);
+  const db = mongoose.connection;
+  db.on("error", console.error.bind(console, "MongoDB connection error:"));
+};
+
+setupDb();
+setupRoutes();
 
 export const server = app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`);
